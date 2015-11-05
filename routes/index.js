ad642124fe7ba4d5ac3c5b9ee24f0c6e00e3f6ad
Nick Alekhine <nick.alekhine@gmail.com>
2015-11-04 22:10:05 -0500

    add history page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'One New Thing' });
+});
+
+/* GET history page. */
+router.get('/history/', function(req, res, next) {
+  res.render('history', { title: 'My History' });
 });
 
 module.exports = router;
