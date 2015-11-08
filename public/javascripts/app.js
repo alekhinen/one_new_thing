1305de43b26bcd07e8ead00392edf29817eaf6e6
Nick Alekhine <nick.alekhine@gmail.com>
2015-11-08 14:18:48 -0500

    more work on filter menu.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,2 @@
 // yo, dawg
+// yep.
--- public/javascripts/filters_menu.js
+++ public/javascripts/filters_menu.js
@@ -1,6 +1,17 @@
+var filterMenuVisible = false;
+
 function toggleSlideFilterMenu() {
-  $('#filter_menu').show('slide', {
-    'direction': 'right',
-    'distance': '200px'
-  }, 1000);
+  if (!filterMenuVisible) {
+    $('#filter_menu').animate({marginLeft: '0px'}, 500);
+    $('#filter_background').css('display', 'block');
+    $('#filter_background').css('background-color', 'rgba(0,0,0,.42)');
+    // TODO: not working :(
+    // $('#filter_background').animate({backgroundColor: 
+    // 'rgba(0,0,0,.42)'}, 500);
+    filterMenuVisible = true;
+  } else {
+    $('#filter_menu').animate({marginLeft: '-200px'}, 500);
+    $('#filter_background').css('display', 'none');
+    filterMenuVisible = false;
+  }
 }
