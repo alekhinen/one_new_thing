6bae54e76dbd79970027aea9c034043d4147f4df
Chris Clark <c.clark1928@gmail.com>
2015-11-11 16:12:13 -0500

    start star ratings in filters and connect page with filters-controller.js

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.history', 'starter.suggestions'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.history', 'starter.suggestions', 'starter.filters'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,7 +29,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.history', 's
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    controller: 'FilterCtrl'
   })
 
   .state('app.search', {
--- www/js/filters/filters-controller.js
+++ www/js/filters/filters-controller.js
@@ -1,5 +1,43 @@
 angular.module('starter.filters', [])
 
 .controller('FilterCtrl', function($scope, $stateParams) {
-	
+
+	var fiveClicked = false;
+	var fourClicked = false;
+	var threeClicked = false;
+	var twoClicked = false;
+	var oneClicked = false;
+
+	var starClickList = [oneClicked, twoClicked, threeClicked, fourClicked, fiveClicked];
+
+	var fiveStarElement = document.getElementById("fiveStarFilter");
+	var fourStarElement = document.getElementById("fourStarFilter");
+	var threeStarElement = document.getElementById("threeStarFilter");
+	var twoStarElement = document.getElementById("twoStarFilter");
+	var oneStarElement = document.getElementById("oneStarFilter");
+
+	var starElementList = [oneStarElement, twoStarElement, threeStarElement, fourStarElement, fiveStarElement];
+
+	$scope.ratingClick = function(starClicked) {
+		starClickList[starClicked] = !starClickList[starClicked];
+
+		if (starClickList[starClicked] == true) {
+			for (i = starClicked; i >= 0; i--) {
+				starElementList[i].classList.add('button-positive');
+				starElementList[i].classList.remove('button-dark');
+				starElementList[i].classList.add('ion-star');
+				starElementList[i].classList.remove('ion-ios-star-outline');
+			}
+		}
+		else {
+			for (i = starClicked; i >= 0; i--) {
+				starElementList[i].classList.remove('button-positive');
+				starElementList[i].classList.add('button-dark');
+				starElementList[i].classList.remove('ion-star');
+				starElementList[i].classList.add('ion-ios-star-outline');
+			}
+		}
+	}
 });
+
+
