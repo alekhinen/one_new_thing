7c2bf55fd8d99e1b0ab8a9a397cbf24924e58bec
Nick Alekhine <nick.alekhine@gmail.com>
2015-11-15 14:09:43 -0500

    more work on reviews page.

--- www/js/reviews/reviews-controller.js
+++ www/js/reviews/reviews-controller.js
@@ -2,6 +2,12 @@ angular.module('starter.reviews', ['ionic'])
 
 .controller('ReviewsCtrl', function($scope) {
   $scope.reviews = [
-
+    {
+      id: 1,
+      reviewer: 'Ariel W.',
+      rating: 5,
+      review: 'Such a sweet place. So much fun. Lol idk this is a thing.',
+      timestamp: '10/10/2015'
+    }
   ];
 });
