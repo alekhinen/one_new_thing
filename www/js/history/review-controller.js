9604e13892bf8945f6f04fe7e8bf6bd397e8934a
Chris Clark <c.clark1928@gmail.com>
2015-11-30 18:40:38 -0500

    Center 'Review This Place' text and implement star rating in review

--- www/js/history/review-controller.js
+++ www/js/history/review-controller.js
@@ -2,13 +2,50 @@ angular.module('starter.review', [])
 
 .controller('ReviewCtrl', function($scope, $stateParams) {
 
+	$scope.newReview = {};
+
+// START: Five star rating thang
+	// Initialization code
+	var fiveStarElement = document.getElementById("fiveStarRating");
+	var fourStarElement = document.getElementById("fourStarRating");
+	var threeStarElement = document.getElementById("threeStarRating");
+	var twoStarElement = document.getElementById("twoStarRating");
+	var oneStarElement = document.getElementById("oneStarRating");
+
+	oneStarElement.classList.add('button-positive');
+	oneStarElement.classList.remove('button-dark');
+	oneStarElement.classList.add('ion-star');
+	oneStarElement.classList.remove('ion-ios-star-outline');
+
+	var starElementList = [oneStarElement, twoStarElement, threeStarElement, fourStarElement, fiveStarElement];
+
+	// Handle
+	$scope.reviewRatingClick = function($event) {
+		starIndex = $event.target.value - 1;
+		for (i = 0; i <= starIndex; i++) {
+			starElementList[i].classList.add('button-positive');
+			starElementList[i].classList.remove('button-dark');
+			starElementList[i].classList.add('ion-star');
+			starElementList[i].classList.remove('ion-ios-star-outline');
+		}
+
+		for (i = starIndex + 1; i < starElementList.length; i++) {
+			starElementList[i].classList.remove('button-positive');
+			starElementList[i].classList.add('button-dark');
+			starElementList[i].classList.remove('ion-star');
+			starElementList[i].classList.add('ion-ios-star-outline');
+		}
+
+		$scope.setStar($event.target.value);
+	}
+
   $scope.range = function(length) {
     return new Array(length);
   };
 
   $scope.setStar = function(starNumber) {
-    console.log(starNumber);
-    $('#review-star-amt').val(starNumber);
+  	console.log(starNumber);
+  	$scope.newReview.userRating = parseInt(starNumber);
   };
 
 });
