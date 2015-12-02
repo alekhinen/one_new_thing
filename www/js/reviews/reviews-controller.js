c4b740c2d6e29bc4231428c3a88420d7b62bbc72
Chris Clark <c.clark1928@gmail.com>
2015-12-01 20:08:56 -0500

    Fix stupid error

--- www/js/reviews/reviews-controller.js
+++ www/js/reviews/reviews-controller.js
@@ -13,7 +13,7 @@ angular.module('starter.reviews', ['ionic'])
       id: 2,
       reviewer: 'Tim B.',
       rating: 3,
-      review: 'I really enjoyed the calmponents of the atmosphere, but it didn\'t tickle my fancy'
+      review: 'I really enjoyed the calmponents of the atmosphere, but it didn\'t tickle my fancy',
       timestamp: '10/1/2015'
     }
   ];
